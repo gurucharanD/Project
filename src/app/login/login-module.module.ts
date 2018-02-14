e1d85fe476868d645bdbecf4b715144f677e6652
gurucharan <gurucharandandyala@gmail.com>
2018-02-14 17:41:20 +0530

    Update login-module.module.ts

--- src/app/login/login-module.module.ts
+++ src/app/login/login-module.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { LoginRoutingModule } from './login-routing.module';
-import { MdlTextFieldModule, MdlButtonModule, MdlCardModule, MdlShadowModule, MdlRadioModule, MdlRadioGroupRegisty } from '@angular-mdl/core';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 
